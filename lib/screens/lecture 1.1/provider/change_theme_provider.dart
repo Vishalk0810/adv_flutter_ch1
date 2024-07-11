a6922975f1dc0a08bc63940b42d6a917d5fd565d
Vishal Keshri <vishalkeshri858@gmail.com>
2024-07-11 15:05:29 +0530

    11-07-2024

--- lib/screens/lecture 1.1/provider/change_theme_provider.dart
+++ lib/screens/lecture 1.1/provider/change_theme_provider.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
 
-class HomeProvider extends ChangeNotifier
-{
-  bool isDarks = false;
+class ChangeThemeScreenProvider extends ChangeNotifier {
+  bool isDark = false;
 
-  void themeChange()
-  {
-    isDarks = !isDarks;
+  bool themeChange() {
+    notifyListeners();
+    return isDark = !isDark;
   }
 
-  void changeThemeData()
-  {
-    ThemeData(
-      colorScheme: ColorScheme.light(
-        brightness: Brightness.light,
-        primary: Colors.purple,
-        secondary: Colors.blue,
-        surface: Colors.pinkAccent,
-      )
-    );
-  }
+  ThemeData changeThemeDataToLight = ThemeData(
+    colorScheme: const ColorScheme(
+      brightness: Brightness.light,
+      primary: Colors.purple,
+      onPrimary: Colors.green,
+      secondary: Colors.blue,
+      onSecondary: Colors.deepOrangeAccent,
+      error: Colors.red,
+      onError: Colors.redAccent,
+      surface: Colors.white,
+      onSurface: Colors.black,
+    ),
+  );
+
+  ThemeData changeThemeDataToDark = ThemeData(
+    colorScheme: const ColorScheme(
+      brightness: Brightness.dark,
+      primary: Colors.amber,
+      onPrimary: Colors.blue,
+      secondary: Colors.pink,
+      onSecondary: Colors.yellowAccent,
+      error: Colors.red,
+      onError: Colors.redAccent,
+      surface: Colors.black,
+      onSurface: Colors.white,
+    ),
+  );
 }
--- lib/screens/lecture 1.1/view/change_theme.dart
+++ lib/screens/lecture 1.1/view/change_theme.dart
@@ -1,54 +1,184 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../provider/change_theme_provider.dart';
 
 class ChangeTheme extends StatelessWidget {
   const ChangeTheme({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    ChangeThemeScreenProvider changeThemeScreenProviderTrue =
+    Provider.of<ChangeThemeScreenProvider>(context, listen: true);
+    ChangeThemeScreenProvider changeThemeScreenProviderFalse =
+    Provider.of<ChangeThemeScreenProvider>(context, listen: false);
     return SafeArea(
       child: Scaffold(
         body: Padding(
-          padding: const EdgeInsets.all(18.0),
+          padding: const EdgeInsets.all(15.0),
           child: Column(
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Icon(Icons.arrow_back_ios_new,size: 28,),
+                  Icon(
+                    Icons.arrow_back_ios,
+                    size: 30,
+                    color: changeThemeScreenProviderTrue.isDark
+                        ? Colors.white
+                        : Colors.grey,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: Icon(Icons.add_circle_outline_outlined,size: 28,),
+                  Icon(
+                    Icons.add_circle_outline,
+                    size: 30,
+                    color: changeThemeScreenProviderTrue.isDark
+                        ? Colors.white
+                        : Colors.grey,
                   ),
                 ],
               ),
-              SizedBox(height: 20,),
-              CircleAvatar(
-                radius: 80,
+              SizedBox(
+                height: height * 0.035,
               ),
-              SizedBox(height: 5,),
-              Text('Vishal Keshri',style: TextStyle(
-                fontSize: 30,
-              ),),
-              Row(
-                children: [
-                  Row(
-                    children: [
-                      SizedBox(height: 50,),
-                      Icon(Icons.dark_mode),
-                      SizedBox(width: 10,),
-                      Text('Light Theme',style: TextStyle(
-                        fontSize: 20,
-                      ),),
-                    ],
-                  )
-                ],
-              )
+              Container(
+                margin: const EdgeInsets.all(10),
+                height: height * 0.2,
+                width: width * 0.6,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                ),
+                child: Image.asset(
+                  'asset/Images/avtar2.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Text(
+                'Vishal Keshri',
+                style: TextStyle(
+                  fontSize: 35,
+                  letterSpacing: 2,
+                ),
+              ),
+              SizedBox(
+                height: height * 0.07,
+              ),
+              ListTile(
+                leading: Icon(
+                  changeThemeScreenProviderTrue.isDark
+                      ? Icons.dark_mode
+                      : Icons.light_mode,
+                  size: 35,
+                  color: changeThemeScreenProviderTrue.isDark
+                      ? changeThemeScreenProviderTrue
+                      .changeThemeDataToDark.colorScheme.primary
+                      : changeThemeScreenProviderTrue
+                      .changeThemeDataToLight.colorScheme.primary,
+                ),
+                title: Text(
+                  changeThemeScreenProviderTrue.isDark
+                      ? 'Dark Mode'
+                      : 'Light Mode',
+                  style: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                trailing: Switch(
+                  activeColor: Colors.yellow,
+                  onChanged: (value) {
+                    changeThemeScreenProviderFalse.themeChange();
+                  },
+                  value: changeThemeScreenProviderTrue.isDark,
+                ),
+              ),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.grid_4x4,
+                  size: 35,
+                  color: changeThemeScreenProviderTrue.isDark
+                      ? changeThemeScreenProviderTrue
+                      .changeThemeDataToDark.colorScheme.onPrimary
+                      : changeThemeScreenProviderTrue
+                      .changeThemeDataToLight.colorScheme.onPrimary,
+                ),
+                title: const Text(
+                  'Story',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.settings,
+                  size: 35,
+                  color: changeThemeScreenProviderTrue.isDark
+                      ? changeThemeScreenProviderTrue
+                      .changeThemeDataToDark.colorScheme.secondary
+                      : changeThemeScreenProviderTrue
+                      .changeThemeDataToLight.colorScheme.secondary,
+                ),
+                title: const Text(
+                  'Settings and Privacy',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.message_outlined,
+                  size: 35,
+                  color: changeThemeScreenProviderTrue.isDark
+                      ? changeThemeScreenProviderTrue
+                      .changeThemeDataToDark.colorScheme.onSecondary
+                      : changeThemeScreenProviderTrue
+                      .changeThemeDataToLight.colorScheme.onSecondary,
+                ),
+                title: const Text(
+                  'Help Center',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.notifications,
+                  size: 35,
+                  color: changeThemeScreenProviderTrue.isDark
+                      ? changeThemeScreenProviderTrue
+                      .changeThemeDataToDark.colorScheme.primary
+                      : changeThemeScreenProviderTrue
+                      .changeThemeDataToLight.colorScheme.primary,
+                ),
+                title: const Text(
+                  'Notification',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
             ],
           ),
-        )
+        ),
       ),
     );
   }
